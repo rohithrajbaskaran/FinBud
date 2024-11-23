155c148d7fd7ff55ca755913edc0e2d09ac48e50
Rohithraj Baskaran <rohithraj.baskaran@Rohithrajs-Laptop.local>
2024-11-22 19:42:03 -0500

    Made some changes to env file

--- front-end/vite.config.js
+++ front-end/vite.config.js
@@ -1,13 +1,26 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   optimizeDeps: {
     include: ['redux-persist', '@reduxjs/toolkit', 'react-redux']
   },
+  build: {
+    outDir: 'dist',
+    sourcemap: true,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom', 'react-router-dom'],
+          redux: ['@reduxjs/toolkit', 'react-redux', 'redux-persist'],
+          charts: ['chart.js', 'react-chartjs-2']
+        }
+      }
+    }
+  },
   server: {
+    port: 3000,
     hmr: {
       overlay: false
     }
